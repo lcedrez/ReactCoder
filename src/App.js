32b41187c304f9e9d33ed332e827eb6e4f3ed870
lcedrez <102386169+lcedrez@users.noreply.github.com>
2022-08-11 00:28:31 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Beatles Store
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://lcedrez.github.io/JavaScriptCoder/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        
+          Store
         </a>
       </header>
     </div>
