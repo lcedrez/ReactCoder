5d0c756aebbd8a355da62d645cd33fdfd4799674
lcedrez <102386169+lcedrez@users.noreply.github.com>
2022-10-08 13:55:58 -0300

    stck

--- src/components/Item/item.js
+++ src/components/Item/item.js
@@ -8,13 +8,13 @@ const   Item=({producto})=>{
 if(producto.stock===0)
 {
         return( 
-                <div className="cardContainer" >
+                <div className="cardContainerStock" >
                         
                         <img src={producto.imagen}/>
                                 <div className='contenedorNombPrec'>
-                                        <div  className='contenCardTit'>
-                                                <h2 className="tituloCard">{producto.nombre}</h2>
-                                                <span className="precioCard">U$S {producto.precio}</span>
+                                        <div  className='contenCardTitStock'>
+                                                <h2 className="tituloCardStock">{producto.nombre}</h2>
+                                        
                                                 <img src='../../../Imagenes/OutStock.jpg'/>
                                         </div>
                                 </div>
